24c124f7e319cc3861c8b95c737acfa1a49b965c
joe.garnett <jgarnett@mtab.com>
2021-12-06 16:23:18 +0000

    day6 part 2 + refactor

--- src/main/java/com/mtab/aventofcode/year2021/day6/Day6.java
+++ src/main/java/com/mtab/aventofcode/year2021/day6/Day6.java
@@ -1,66 +1,48 @@
 package com.mtab.aventofcode.year2021.day6;
 
-import com.google.common.base.Preconditions;
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
 import com.mtab.aventofcode.models.InputLoader;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 public class Day6 implements
         InputLoader<List<Integer>>,
-        Supplier<Integer> {
+        Supplier<Long> {
 
     private static final String INPUT_PATH = "2021/day6/input.txt";
-    private static final int CYCLES = 80;
-    private static final int SPAWN_CYCLE = 7;
-    private static final int FIRST_SPAWN_OFFSET = 2;
+    private static final int CYCLES = 256;
 
-    // TODO: int[9] add fish to array pos, each cycle shift down 1 pos - take all in 0 and add to total
-    
     private final List<Integer> input;
 
     Day6() {
         this.input = this.getInput(INPUT_PATH);
     }
 
-    private int spawn(final int t) {
-        return this.spawn(t + 1, new AtomicInteger(1), CYCLES);
-    }
+    private long compute() {
+        long fishes = this.input.size();
 
-    // TODO: eliminate recursion
-    private int spawn(final int timeToFirstSpawn, final AtomicInteger acc, final int cycles) {
-        final int remainingCycles = cycles - timeToFirstSpawn;
+        final long[] days = new long[9];
+        this.input.forEach(i -> days[i]++);
 
-        if (remainingCycles < 0) {
-            return acc.get();
+        for (int i = 0; i < CYCLES; i++) {
+            final long fishToAdd = days[0];
+            ArrayUtils.shift(days, -1);
+            fishes += fishToAdd;
+            days[6] += days[days.length -1] = fishToAdd;
         }
 
-        final int spawn = remainingCycles / SPAWN_CYCLE + 1;
-
-        acc.addAndGet(spawn);
-
-        for (int i = 0; i < spawn; ++i) {
-            this.spawn(
-                    SPAWN_CYCLE + FIRST_SPAWN_OFFSET,
-                    acc,
-                    remainingCycles - (i * SPAWN_CYCLE));
-        }
-
-        return acc.get();
+        return fishes;
     }
 
     @Override
-    public Integer get() {
-        return this.input
-                .parallelStream()
-                .mapToInt(this::spawn)
-                .sum();
+    public Long get() {
+        return this.compute();
     }
 
     @Override
@@ -81,9 +63,9 @@ public class Day6 implements
 
     public static void main(final String[] args) {
         final Stopwatch sw = Stopwatch.createStarted();
-        final int result = new Day6().get();
+        final long result = new Day6().get();
 
-        Preconditions.checkArgument(result == 360268);
+        // Preconditions.checkArgument(result == 360268);
 
         System.out.println(result);
         System.out.printf("Execution time: %dms%n", sw.elapsed(TimeUnit.MILLISECONDS));

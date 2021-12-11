0da6ba5ffcbbe6f1f0c2ad484d706554b635dafa
josephgarnett <josephgarnett@gmail.com>
2021-12-11 16:10:29 +0000

    day 11

---
+++ src/main/java/com/mtab/aventofcode/year2021/day11/Day11.java
@@ -0,0 +1,62 @@
+package com.mtab.aventofcode.year2021.day11;
+
+import com.google.common.base.Stopwatch;
+import com.mtab.aventofcode.models.InputGridLoader;
+
+import java.awt.geom.Point2D;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
+
+public class Day11 implements
+        InputGridLoader<Octopus, OctopusGrid>,
+        Supplier<Long> {
+
+    private static final int CYCLES = 1000;
+
+    private OctopusGrid input;
+    private final AtomicLong flashes = new AtomicLong(0);
+
+    public Day11(final String resourcePath) {
+        this.input = this.getInput(resourcePath);
+    }
+
+    private int compute(final OctopusGrid grid) {
+        return grid.computeFlashes();
+    }
+
+    @Override
+    public Long get() {
+        for (int i = 0; i < CYCLES; i++) {
+            final int flashes = this.compute(this.input);
+
+            this.flashes.addAndGet(flashes);
+
+            if (flashes == this.input.size()) {
+                System.out.println("Synchronization: " + (i + 1));
+                break;
+            }
+        }
+
+        return this.flashes.get();
+    }
+
+    @Override
+    public Octopus parseElement(final String e, final int x, final int y) {
+        return new Octopus(Integer.parseInt(e), new Point2D.Float(x, y));
+    }
+
+    @Override
+    public OctopusGrid createGrid(List<Octopus> grid) {
+        return new OctopusGrid(grid);
+    }
+
+    public static void main(final String... args) {
+        final Stopwatch sw = Stopwatch.createStarted();
+        final long result = new Day11("2021/day11/input.txt").get();
+
+        System.out.println(result);
+        System.out.printf("Execution time: %dms%n", sw.elapsed(TimeUnit.MILLISECONDS));
+    }
+}
---
+++ src/main/java/com/mtab/aventofcode/year2021/day11/Octopus.java
@@ -0,0 +1,46 @@
+package com.mtab.aventofcode.year2021.day11;
+
+import com.mtab.aventofcode.models.grid.GridPoint;
+
+import java.awt.geom.Point2D;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Octopus implements GridPoint<AtomicInteger> {
+
+    private final AtomicInteger value;
+    private final Point2D position;
+
+    public Octopus(final int value, final Point2D position) {
+        this.value = new AtomicInteger(value);
+        this.position = position;
+    }
+
+    public int flash(final OctopusGrid grid) {
+        final int value = this.value.incrementAndGet();
+
+        if (value == 10) {
+            return grid.getNeighbours(this)
+                    .stream()
+                    .mapToInt(o -> o.flash(grid))
+                    .sum() + 1;
+        }
+
+        return 0;
+    }
+
+    public void reset() {
+        if (this.getValue().get() >= 10) {
+            this.getValue().set(0);
+        }
+    }
+
+    @Override
+    public AtomicInteger getValue() {
+        return this.value;
+    }
+
+    @Override
+    public Point2D getPosition() {
+        return this.position;
+    }
+}

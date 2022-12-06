5f34afde10a7e7f047a25cd7ed4ad6eea864949c
josephgarnett <josephgarnett@gmail.com>
2022-12-06 08:08:19 +0000

    2022 Day 6 part 1

---
+++ src/main/java/com/mtab/aventofcode/year2022/day6/Day6.java
@@ -0,0 +1,59 @@
+package com.mtab.aventofcode.year2022.day6;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Stopwatch;
+import com.mtab.aventofcode.utils.InputUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.BitSet;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Day6 implements Function<String, Integer> {
+    private static String getInput() throws IOException {
+        return Files.lines(
+                        Path.of(InputUtils.getInputPath("2022/day6/input.txt")))
+                .collect(Collectors.joining());
+    }
+
+    public static void main(final String[] args) throws IOException {
+        final Stopwatch sw = Stopwatch.createStarted();
+
+        final var result = new Day6().apply(Day6.getInput());
+
+        System.out.println(result);
+        System.out.printf("Execution time: %dms%n", sw.elapsed(TimeUnit.MILLISECONDS));
+
+        Preconditions.checkArgument(result == 2276);
+    }
+
+    @Override
+    public Integer apply(final String signal) {
+
+
+        for (int i = 0; i < signal.length(); ++i) {;
+            final String behind = signal.substring(Math.max(i - 3, 0), i + 1);
+            final String ahead = signal.substring(i, Math.min(i + 4, signal.length()));
+            final BitSet bitSet = new BitSet();
+
+            behind.chars().forEach(bitSet::set);
+
+            if (bitSet.cardinality() > 3) {
+                return i;
+            }
+
+            bitSet.clear();
+
+            ahead.chars().forEach(bitSet::set);
+
+            if (bitSet.cardinality() > 3) {
+                return i + ahead.length();
+            }
+        }
+
+        return -1;
+    }
+}

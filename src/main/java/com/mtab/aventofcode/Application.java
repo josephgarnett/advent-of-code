4eed08c99ed53b1ab7179e2becadd03045d87e75
josephgarnett <josephgarnett@gmail.com>
2022-12-07 19:12:46 +0000

    extra analytics for running

--- src/main/java/com/mtab/aventofcode/Application.java
+++ src/main/java/com/mtab/aventofcode/Application.java
@@ -1,15 +1,19 @@
 package com.mtab.aventofcode;
 
+import com.google.common.base.Preconditions;
 import com.google.common.base.Stopwatch;
 import com.mtab.aventofcode.utils.TaskUtils;
 import org.fusesource.jansi.AnsiConsole;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import static org.fusesource.jansi.Ansi.ansi;
 
 public class Application {
+    private static final int DEFAULT_TEST_RUNS = 500;
     public static void main(String... args) {
         AnsiConsole.systemInstall();
     }
@@ -17,6 +21,16 @@ public class Application {
     public static <T> T challenge(
             final String baseResourcePath,
             final Callable<T> task) throws Exception {
+        return Application.challenge(
+                baseResourcePath,
+                task,
+                DEFAULT_TEST_RUNS);
+    }
+
+    public static <T> T challenge(
+            final String baseResourcePath,
+            final Callable<T> task,
+            final int tests) throws Exception {
         try {
             System.out.println(
                     ansi().fgBrightBlue()
@@ -29,15 +43,22 @@ public class Application {
                     .reset());
         }
 
-        final Stopwatch sw = Stopwatch.createStarted();
-
+        final List<Long> times = new ArrayList<>(tests);
         final var result = task.call();
+
+        for (int i = 1; i < tests; ++i) {
+            final Stopwatch sw = Stopwatch.createStarted();
+            Preconditions.checkArgument(task.call().equals(result));
+            times.add(sw.elapsed(TimeUnit.MILLISECONDS));
+        }
 
         System.out.println(result);
         System.out.println("============================================");
         System.out.print(ansi()
                 .fgBrightGreen()
-                .a(String.format("Execution time: %dms%n", sw.elapsed(TimeUnit.MILLISECONDS)))
+                .a(String.format("Average execution time over %d runs: %.4fms%n",
+                        tests,
+                        times.stream().mapToLong(t -> t).average().orElseThrow()))
                 .reset());
         System.out.println("============================================\n\n");
 

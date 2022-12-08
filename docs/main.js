86b9db3b22154f53dc00c82ee248e6ccb59a570e
josephgarnett <josephgarnett@gmail.com>
2022-12-08 20:59:28 +0000

    fixed id

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,7 @@
           return response.text();
         })
         .then((report) => {
-           document.getElementById('#root')
+           document.getElementById('root')
             .textContent = report;
         });
 })();

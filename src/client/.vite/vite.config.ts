f1ceede0b277b1f9f388a99e019aabbbbc53a326
josephgarnett <josephgarnett@gmail.com>
2023-12-02 23:53:34 +0000

    new client build

--- src/client/.vite/vite.config.ts
+++ src/client/.vite/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// @ts-ignore
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, '..', '..', '..', 'docs')
+  },
   plugins: [react()],
 })

a654be7c7f482510701669a56dbdbec4929e4b59
josephgarnett <josephgarnett@gmail.com>
2023-12-02 00:27:10 +0000

    2015 day 3

--- src/main/java/com/mtab/adventofcode/year2015/Challenge2015.java
+++ src/main/java/com/mtab/adventofcode/year2015/Challenge2015.java
@@ -2,6 +2,7 @@ package com.mtab.adventofcode.year2015;
 
 import com.google.common.base.Stopwatch;
 import com.mtab.adventofcode.year2015.day2.Day2;
+import com.mtab.adventofcode.year2015.day3.Day3;
 import com.mtab.adventofcode.year2023.day1.Day1;
 
 import java.util.concurrent.TimeUnit;
@@ -14,6 +15,7 @@ public class Challenge2015 {
 
         Day1.main(args);
         Day2.main(args);
+        Day3.main(args);
 
         System.out.print(
                 ansi()

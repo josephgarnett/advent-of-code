52f0fa7f1eae4d8e957f3a36d5570050b202b355
josephgarnett <josephgarnett@gmail.com>
2023-12-03 23:04:43 +0000

    switch to hash router

--- src/client/src/routes/router.tsx
+++ src/client/src/routes/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import { AppRoute } from "./app";
 import { AdventOfCodeRoute } from "./adventOfCode";
 
-export const router = createBrowserRouter([{
+export const router = createHashRouter([{
   path: "/",
   element: <AppRoute />,
   children: [{

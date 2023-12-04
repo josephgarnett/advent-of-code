859f37166d380e75b799e582c05a39687401eb7d
josephgarnett <josephgarnett@gmail.com>
2023-12-04 09:41:20 +0000

    2023 day 4 part 2

--- src/main/java/com/mtab/adventofcode/year2023/day4/Day4.java
+++ src/main/java/com/mtab/adventofcode/year2023/day4/Day4.java
@@ -4,13 +4,11 @@ import com.google.common.base.Preconditions;
 import com.mtab.adventofcode.Application;
 import com.mtab.adventofcode.utils.InputUtils;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -18,6 +16,37 @@ public class Day4 implements Function<List<Day4.Scratchcard>, Long> {
 
     @Override
     public Long apply(
+            @NonNull final List<Scratchcard> scratchcards) {
+        final Map<Scratchcard, Long> scratchcardResults = new HashMap<>();
+        scratchcards.forEach(s -> this.get(s, scratchcards, scratchcardResults));
+
+        return scratchcardResults.values()
+                .stream()
+                .mapToLong(t -> t)
+                .sum();
+    }
+
+    private void get(
+            @NonNull final Scratchcard scratchcard,
+            @NonNull final List<Scratchcard> scratchcards,
+            @NonNull final Map<Scratchcard, Long> results) {
+        results.computeIfPresent(scratchcard, (k, v) -> v + 1);
+        results.computeIfAbsent(scratchcard, k -> 1L);
+
+        if (scratchcard.getWinners() == 0) {
+            return;
+        }
+
+        final long value = scratchcard.getWinners();
+        final int position = scratchcards.indexOf(scratchcard);
+
+        for (int i = 0; i < value; i++) {
+            final Scratchcard next = scratchcards.get(position + i + 1);
+            this.get(next, scratchcards, results);
+        }
+    }
+
+    private long part1(
             @NonNull final List<Scratchcard> scratchcards) {
         return scratchcards
                 .stream()
@@ -52,22 +81,40 @@ public class Day4 implements Function<List<Day4.Scratchcard>, Long> {
         final var result = Application.challenge(
                 "2023/day4",
                 () -> new Day4()
-                        .apply(Day4.getInput()));
+                        .apply(Day4.getInput()),
+                50);
 
-        Preconditions.checkArgument(result == 17803);
+        Preconditions.checkArgument(result == 5554894);
     }
 
-    @Builder
-    public record Scratchcard(String id, Set<Long> numbers, Set<Long> winningNumbers) {
+    @Getter
+    public static class Scratchcard {
+        private final String id;
+        private final Set<Long> numbers;
+        private final Set<Long> winningNumbers;
+        private final long winners;
 
-        public long getWinning() {
-            final Set<Long> intersection = new HashSet<>(this.winningNumbers());
-            intersection.retainAll(this.numbers());
+        @Builder
+        public Scratchcard(
+                @NonNull final String id,
+                @NonNull final Set<Long> numbers,
+                @NonNull final Set<Long> winningNumbers) {
+            this.id = id;
+            this.numbers = numbers;
+            this.winningNumbers = winningNumbers;
+            this.winners = this.computeWinners(numbers, winningNumbers);
+        }
+
+        private long computeWinners(
+                @NonNull final Set<Long> numbers,
+                @NonNull final Set<Long> winningNumbers) {
+            final Set<Long> intersection = new HashSet<>(winningNumbers);
+            intersection.retainAll(numbers);
 
             return intersection.size();
         }
         public long getWinningValue() {
-            return (long) Math.pow(2, this.getWinning() - 1);
+            return (long) Math.pow(2, this.getWinners() - 1);
         }
     }
 }

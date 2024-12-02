17beeff933a05c019bfec7f7fbba9ae82cb06372
Joe Garnett <jgarnett@mtab.com>
2024-12-02 11:50:13 +0000

    2024 day 1

---
+++ src/main/java/io/garnett/adventofcode/year2024/Challenge2024.java
@@ -0,0 +1,24 @@
+package io.garnett.adventofcode.year2024;
+
+import com.google.common.base.Stopwatch;
+import io.garnett.adventofcode.year2024.day1.Day1;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class Challenge2024 {
+    public static void main(final String[] args) throws Exception {
+        final Stopwatch sw = Stopwatch.createStarted();
+
+        Day1.main(args);
+
+        System.out.print(
+                ansi()
+                        .fgYellow()
+                        .a(String.format(
+                                "\uD83C\uDF89\uD83C\uDF89 All challenges run, execution time: %dms%n",
+                                sw.elapsed(TimeUnit.MILLISECONDS)))
+                        .reset());
+    }
+}
---
+++ src/main/java/io/garnett/adventofcode/year2024/day1/Day1.java
@@ -0,0 +1,74 @@
+package io.garnett.adventofcode.year2024.day1;
+
+import com.google.common.base.Preconditions;
+import io.garnett.adventofcode.Application;
+import io.garnett.adventofcode.utils.InputUtils;
+import lombok.NonNull;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day1 implements ToLongFunction<List<ImmutablePair<Integer, Integer>>> {
+
+    private static final long PART_1_RESULT = 1722302;
+
+    @Override
+    public long applyAsLong(
+            @NonNull final List<ImmutablePair<Integer, Integer>> input) {
+        return this.part2(input);
+    }
+
+    private long part2(
+            @NonNull final List<ImmutablePair<Integer, Integer>> input) {
+        final Map<Integer, List<Integer>> right = input.stream()
+                .map(ImmutablePair::getRight)
+                .collect(Collectors.groupingBy(Function.identity()));
+
+        return input.stream()
+                .map(ImmutablePair::getLeft)
+                .mapToInt(t -> t * right.getOrDefault(t, List.of()).size())
+                .sum();
+    }
+
+    private long part1(
+            @NonNull final List<ImmutablePair<Integer, Integer>> input) {
+        final List<Integer> left = input.stream()
+                .map(ImmutablePair::getLeft)
+                .sorted()
+                .toList();
+
+        final List<Integer> right = input.stream()
+                .map(ImmutablePair::getRight)
+                .sorted()
+                .toList();
+
+        return IntStream.range(0, Math.min(left.size(), right.size()))
+                .map(i -> Math.abs(left.get(i) - right.get(i)))
+                .sum();
+    }
+
+    private static List<ImmutablePair<Integer, Integer>> getInput() throws IOException {
+        return InputUtils.readLines(
+                        "2024/day1/input.txt")
+                .map(t ->  t.split("\\s+"))
+                .map(t -> new ImmutablePair<>(
+                        Integer.parseInt(t[0]),
+                        Integer.parseInt(t[1])))
+                .toList();
+    }
+
+    public static void main(final String[] args) {
+        final var result = Application.challenge(
+                "2024/day1",
+                () -> new Day1()
+                        .applyAsLong(Day1.getInput()));
+
+        Preconditions.checkArgument(result == PART_1_RESULT);
+    }
+}

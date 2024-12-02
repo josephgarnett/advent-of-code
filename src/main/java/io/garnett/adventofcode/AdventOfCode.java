ededcd8bee1870e094a65faf79b2ace29f8af192
Joe Garnett <jgarnett@mtab.com>
2024-12-02 17:57:03 +0000

    update switch

--- src/main/java/io/garnett/adventofcode/AdventOfCode.java
+++ src/main/java/io/garnett/adventofcode/AdventOfCode.java
@@ -2,6 +2,7 @@ package io.garnett.adventofcode;
 
 import io.garnett.adventofcode.year2022.Challenge2022;
 import io.garnett.adventofcode.year2023.Challenge2023;
+import io.garnett.adventofcode.year2024.Challenge2024;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
@@ -17,6 +18,10 @@ public class AdventOfCode {
             final String year = cmd.getOptionValue("y");
 
             switch (year) {
+                case "2024": {
+                    Challenge2024.main(args);
+                    break;
+                }
                 case "2023": {
                     Challenge2023.main(args);
                     break;

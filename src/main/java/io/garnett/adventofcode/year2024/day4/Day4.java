6bf7c27861bc0c7047643258ee608997fea919fc
Joe Garnett <jgarnett@mtab.com>
2024-12-04 10:43:23 +0000

    2024 Day 4 Part 2

--- src/main/java/io/garnett/adventofcode/year2024/day4/Day4.java
+++ src/main/java/io/garnett/adventofcode/year2024/day4/Day4.java
@@ -16,31 +16,82 @@ import java.util.function.Function;
 import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Day4 implements ToLongFunction<Map<Point, String>> {
 
-    private static final String TARGET = "XMAS";
+    private static final String P1_TARGET = "XMAS";
+    private static final String P2_TARGET = "MAS";
+    private static final String P2_TARGET_ALT = "SAM";
+
 
     @Override
     public long applyAsLong(
             @NonNull final Map<Point, String> input) {
+        return this.part2(input);
+    }
+
+    private long part2(
+            @NonNull final Map<Point, String> input) {
+        return input.entrySet()
+                .stream()
+                .filter(t -> StringUtils.equals("A", t.getValue()))
+                .mapToLong(this.mapToX(input))
+                .sum();
+    }
+
+    private long part1(
+            @NonNull final Map<Point, String> input) {
         return input.entrySet()
                 .stream()
                 .filter(t -> StringUtils.equals("X", t.getValue()))
-                .mapToLong(this.mapToMatches(TARGET, input))
+                .mapToLong(this.mapToMatches(P1_TARGET, input))
                 .sum();
+    }
+
+    private ToLongFunction<Map.Entry<Point, String>> mapToX(
+            @NonNull final Map<Point, String> map) {
+        return entry -> Stream.of(
+                        Direction.NORTH_EAST,
+                        Direction.NORTH_WEST)
+                .map(direction -> this.buildFromCenter(entry.getKey(), direction, map, 1))
+                .allMatch(s -> StringUtils.equals(P2_TARGET, s) || StringUtils.equals(P2_TARGET_ALT, s)) ? 1 : 0;
     }
 
     private ToLongFunction<Map.Entry<Point, String>> mapToMatches(
             @NonNull final String target,
             @NonNull final Map<Point, String> map) {
         return entry -> Arrays.stream(Direction.values())
-                .map(direction -> this.build(entry.getKey(), direction, map, target.length()))
+                .map(direction -> this.buildFromStart(entry.getKey(), direction, map, target.length()))
                 .filter(s -> StringUtils.equals(target, s))
                 .count();
     }
 
-    private String build(
+    private String buildFromCenter(
+            @NonNull final Point start,
+            @NonNull final Direction direction,
+            @NonNull final Map<Point, String> map,
+            int length) {
+        final StringBuilder builder = new StringBuilder(length);
+
+        builder.append(map.get(start));
+
+        for (int i = 1; i < length + 1; ++i) {
+            if (direction == Direction.NORTH_EAST) {
+                builder.insert(0, map.get(new Point(start.x + i, start.y - i)));
+                builder.append(map.get(new Point(start.x - i, start.y + i)));
+            }
+
+            if (direction == Direction.NORTH_WEST) {
+                builder.insert(0, map.get(new Point(start.x - i, start.y - i)));
+                builder.append(map.get(new Point(start.x + i, start.y + i)));
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private String buildFromStart(
             @NonNull final Point start,
             @NonNull final Direction direction,
             @NonNull final Map<Point, String> map,
@@ -91,6 +142,6 @@ public class Day4 implements ToLongFunction<Map<Point, String>> {
                 () -> new Day4()
                 .applyAsLong(Day4.getInput()));
 
-        Preconditions.checkArgument(result == 2603);
+        Preconditions.checkArgument(result == 1965);
     }
 }
